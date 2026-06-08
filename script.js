2572a66673abc19068f0f7bdc2bb809c01709332
oliverfanfofthansen <oliverfanfofthansen@icloud.com>
2026-06-08 11:35:31 +0200

    Refactor color toggle logic in byttFarge function

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let boks = document.getElementById("boks");
 
 function byttFarge() {
-    if (boks.style.backgroundColor === "rgb(38, 70, 83)") {
-        boks.style.backgroundColor = "rgb(233, 196, 106)";
+    // Sjekkar om fargen ikkje er den gule. Viss den ikkje er gul, gjer me den gul.
+    if (boks.style.backgroundColor !== "rgb(233, 196, 106)") {
+        boks.style.backgroundColor = "rgb(233, 196, 106)"; // Ein fin, varm gul/oransje
     } else {
-        boks.style.backgroundColor = "rgb(38, 70, 83)";
+        boks.style.backgroundColor = "rgb(38, 70, 83)";  // Ein moderne mørk sjøgrøn
     }
 }
