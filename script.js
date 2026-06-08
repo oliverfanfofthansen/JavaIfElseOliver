9b8e2527e4df4ac962d2ed0def2f02dcf456b975
oliverfanfofthansen <oliverfanfofthansen@icloud.com>
2026-06-08 11:20:16 +0200

    Add color toggle function for element 'boks'

---
+++ script.js
@@ -0,0 +1,9 @@
+let boks = document.getElementById("boks");
+
+function byttFarge() {
+    if (boks.style.backgroundColor === "rgb(38, 70, 83)") {
+        boks.style.backgroundColor = "rgb(233, 196, 106)";
+    } else {
+        boks.style.backgroundColor = "rgb(38, 70, 83)";
+    }
+}
